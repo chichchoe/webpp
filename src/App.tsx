0ad781425b60d478218c9e1925762692b5d76a89
tien nguyen <tien.nv@csell.com.vn>
2022-03-03 18:12:12 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Manatee from './components/Manatee';
+import Narwhal from './components/Narwhal';
 function App() {
     return (
         <div className="wrapper">
             <h1>Marine Mammals</h1>
+
+            <Routes>
+                <Route path="/" element={<Manatee />} />
+                <Route path="about" element={<Narwhal />} />
+            </Routes>
         </div>
     );
 }
--- src/components/Manatee.tsx
+++ src/components/Manatee.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Manatee() {
-    return <h2>Manatee</h2>;
+    return (
+        <>
+            <main>
+                <h2>Welcome to the homepage!</h2>
+                <p>You can do this, I believe in you.</p>
+            </main>
+            <nav>
+                <Link to="/about">About</Link>
+            </nav>
+        </>
+    );
 }
